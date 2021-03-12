496eb703a85366e7a17b7ff71c48b61f75a8ddd5
mukesh shelke <67261625+mukesh2309@users.noreply.github.com>
2021-03-12 19:27:38 +0530

    Add files via upload
    done

---
+++ index.php
@@ -0,0 +1,52 @@
+<!Doctype html>
+<html>
+<head>
+    <title></title>
+    <style>
+        div.main{
+            display: flex;
+            justify-content: space-around;
+            background-color: purple;
+            padding: 1rem;
+
+        }
+        div.main>div{
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
+
+
+<?php
+
+function myTest(){
+    echo 
+    "
+    <div class = main >
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    <div>
+      <h1>MUKESH</h1>
+    </div>
+    </div>";
+ 
+    myTest();
+}
+myTest();
+?>
+</body>
+</html>
